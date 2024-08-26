0f2319ffb42eb5883c2ea49b96e510f1b1a0b8d0
Namani31 <kwg531@naver.com>
2024-08-26 17:27:46 +0900

    Feat: 초기 변수와 메뉴 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,17 @@ package org.example;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        boolean whileFlag = true;
+        String[] foodList = {"비빔밥", "된장찌개", "꽁치찌개", "김밥", "떡볶이", "삼겹살", "순두부찌개", "냉면", "해물파전", "삼계탕", "죽"};
+
+        System.out.println("-----------------------------");
+        System.out.println("음식 랜덤 추천");
+        System.out.println("1. 랜덤 뽑기");
+        System.out.println("입력: ");
+        userInput.nextLine();
+
     }
 }
