186681006d76f6c918a0fddb086335fa8c440615
Namani31 <kwg531@naver.com>
2024-08-26 17:32:55 +0900

    Feat: 랜덤 출력 추가
    - 유저의 메뉴 선택 값을 받을 `userChoice` 추가
    - Random 객체 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,29 @@
 package org.example;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
+        int userChoice = 0;
         boolean whileFlag = true;
         String[] foodList = {"비빔밥", "된장찌개", "꽁치찌개", "김밥", "떡볶이", "삼겹살", "순두부찌개", "냉면", "해물파전", "삼계탕", "죽"};
+        Random rand = new Random();
 
         System.out.println("-----------------------------");
         System.out.println("음식 랜덤 추천");
         System.out.println("1. 랜덤 뽑기");
         System.out.println("입력: ");
-        userInput.nextLine();
+        userChoice = userInput.nextInt();
 
+        while (whileFlag) {
+            switch (userChoice) {
+                case 1:
+                    System.out.println(foodList[rand.nextInt(11)]);
+
+
+            }
+        }
     }
 }
