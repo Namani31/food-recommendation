e90764887aef279bc10c57fc5b37c9ef7a26698c
Namani31 <kwg531@naver.com>
2024-08-28 16:13:50 +0900

    Fix: 메뉴 선택 후 loop 되던 것 해결

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,20 +12,23 @@ public class Main {
         String[] japaneseFoodList = {"Sushi", "Ramen", "Tempura", "Wagyu", "Udon", "Gyoza ", "Takoyaki"};
         Random rand = new Random();
 
-        System.out.println("-----------------------------");
-        System.out.println("Random food recommendation");
-        System.out.println("1. Korean Food | 2. Japanese Food | 3. Chinese Food | 4. Western Food | 5. Quit");
-        System.out.println("-----------------------------");
-        System.out.print("user Input: ");
-        userChoice = userInput.nextInt();
-
         while (whileFlag) {
+            System.out.println("-----------------------------");
+            System.out.println("Random food recommendation");
+            System.out.println("1. Korean Food | 2. Japanese Food | 3. Chinese Food | 4. Western Food | 5. Quit");
+            System.out.println("-----------------------------");
+            System.out.print("user Input: ");
+            userChoice = userInput.nextInt();
+
+
             switch (userChoice) {
                 case 1:
                     System.out.println(koreanFoodList[rand.nextInt(7)]);
+                    System.out.println();
                     break;
                 case 2:
                     System.out.println(japaneseFoodList[rand.nextInt(7)]);
+                    System.out.println();
                     break;
                 case 5:
                     whileFlag = false;
